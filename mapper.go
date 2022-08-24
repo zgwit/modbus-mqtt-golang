4e1a2775045a13a095717c25b3fa29f9bf8dd2d2
Jason <jason@zgwit.com>
2022-08-24 23:56:58 +0800

    先提交一下

---
+++ mapper.go
@@ -0,0 +1,1 @@
+package modbus_mqtt
--- modbus.go
+++ modbus.go
@@ -19,3 +19,12 @@ func modbusOpen() error {
 func ttt() {
 	//buf, err := modbusClient.ReadCoils(0, 10)
 }
+
+type Modbus struct {
+	client  modbus.Client
+	devices []Device
+}
+
+func (m *Modbus) Load(cfg string) {
+
+}
